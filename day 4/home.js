b8dfde2cc66f8a43dae16ef704989a23d07a38f8
Gim_Hong <ragnogk@gmail.com>
2018-12-01 16:23:37 +0800

    day4_5

---
+++ day 4/home.js
@@ -0,0 +1,39 @@
+var my_name;
+var my_age;
+var my_birthdate;
+
+my_name = "Stackup";
+my_age = 23;
+my_birthday = "20/03/1988"
+
+console.log(my_name);
+console.log(my_age);
+console.log(my_birthday);
+
+var my_address = "JTC LaunchPad";
+
+var my_test;
+
+console.log( typeof(my_test));
+console.log(typeof(my_name));
+console.log(typeof(my_age));
+
+var firstName;
+var lastName;
+
+firstName = "Nick";
+lastName = "Test";
+space = " ";
+
+var fullName = firstName + space + lastName;
+
+var fullName1 = firstName + 5 + lastName;
+
+console.log(fullName);
+console.log(fullName1);
+
+var qty = 2;
+var total = 200;
+var qtyTotal = qty + total;
+
+console.log(qtyTotal);
